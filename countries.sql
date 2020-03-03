049c0617887ac25a8c472397b1d39ac962000d0f
Sejin Kim <kim3@kenyon.edu>
2020-03-03 12:00:20 -0500

    Update countries.sql

--- countries.sql
+++ countries.sql
@@ -1,86 +1,86 @@
-CREATE TABLE cities (
-  name                    VARCHAR   PRIMARY KEY,
-  country_code            VARCHAR,
-  city_proper_pop         REAL,
-  metroarea_pop           REAL,
-  urbanarea_pop           REAL
+CREATE TABLE `cities` (
+  `ID` varchar(40) NOT NULL PRIMARY KEY,
+  `country_code` varchar(3),
+  `city_proper_pop` real,
+  `metroarea_pop` real,
+  `urbanarea_pop` real
 );
 
 CREATE TABLE countries (
-  code                  VARCHAR     PRIMARY KEY,
-  name                  VARCHAR,
-  continent             VARCHAR,
-  region                VARCHAR,
-  surface_area          REAL,
-  indep_year            INTEGER,
-  local_name            VARCHAR,
-  gov_form              VARCHAR,
-  capital               VARCHAR,
-  cap_long              REAL,
-  cap_lat               REAL
+  `code` varchar(3) NOT NULL PRIMARY KEY,
+  `name` varchar(50),
+  `continent` varchar(40),
+  `region` varchar(50),
+  `surface_area` real,
+  `indep_year` int(4),
+  `local_name` varchar(70),
+  `gov_form` varchar(50),
+  `capital` varchar(50),
+  `cap_long` real,
+  `cap_lat` real
 );
 
 CREATE TABLE languages (
-  lang_id               INTEGER     PRIMARY KEY,
-  code                  VARCHAR,
-  name                  VARCHAR,
-  percent               REAL,
-  official              BOOLEAN
+  `lang_id` int(3) PRIMARY KEY,
+  `code` varchar(3),
+  `name` varchar(50),
+  `percent` real,
+  `official` boolean
 );
 
 CREATE TABLE economies (
-  econ_id               INTEGER     PRIMARY KEY,
-  code                  VARCHAR,
-  year                  INTEGER,
-  income_group          VARCHAR,
-  gdp_percapita         REAL,
-  gross_savings         REAL,
-  inflation_rate        REAL,
-  total_investment      REAL,
-  unemployment_rate     REAL,
-  exports               REAL,
-  imports               REAL
+  `econ_id` int(3),
+  `code` varchar(3),
+  `year` int(4),
+  `income_group` varchar(30),
+  `gdp_percapita` real,
+  `gross_savings` real,
+  `inflation_rate` real,
+  `total_investment` real,
+  `unemployment_rate` real,
+  `exports` real,
+  `imports` real
 );
 
 CREATE TABLE currencies (
-  curr_id               INTEGER     PRIMARY KEY,
-  code                  VARCHAR,
-  basic_unit            VARCHAR,
-  curr_code             VARCHAR,
-  frac_unit             VARCHAR,
-  frac_perbasic         REAL
+  `curr_id` int(3) PRIMARY KEY,
+  `code` varchar(3),
+  `basic_unit` varchar(50),
+  `curr_code` varchar(3),
+  `frac_unit` varchar(50),
+  `frac_perbasic` real
 );
 
 CREATE TABLE populations (
-  pop_id                INTEGER     PRIMARY KEY,
-  country_code          VARCHAR,
-  year                  INTEGER,
-  fertility_rate        REAL,
-  life_expectancy       REAL,
-  size                  REAL
+  `pop_id` int(3) PRIMARY KEY,
+  `country_code` varchar(3),
+  `year` int(4),
+  `fertility_rate` real,
+  `life_expectancy` real,
+  `size` real
 );
 
 
 CREATE TABLE countries_plus (
-  name                  VARCHAR,
-  continent             VARCHAR,
-  code                  VARCHAR     PRIMARY KEY,
-  surface_area          REAL,
-  geosize_group         VARCHAR
+  `name                  VARCHAR,
+  `continent             VARCHAR,
+  `code                  VARCHAR     PRIMARY KEY,
+  `surface_area          REAL,
+  `geosize_group         VARCHAR
 );
 
 CREATE TABLE economies2010 (
-  code                  VARCHAR     PRIMARY KEY,
-  year                  INTEGER,
-  income_group          VARCHAR,
-  gross_savings         REAL
+  `code                  VARCHAR     PRIMARY KEY,
+  `year                  INTEGER,
+  `income_group          VARCHAR,
+  `gross_savings         REAL
 );
 
 CREATE TABLE economies2015 (
-  code                  VARCHAR     PRIMARY KEY,
-  year                  INTEGER,
-  income_group          VARCHAR,
-  gross_savings         REAL
+  `code                  VARCHAR     PRIMARY KEY,
+  `year                  INTEGER,
+  `income_group          VARCHAR,
+  `gross_savings         REAL
 );
 
 -- Copy over data from CSVs
